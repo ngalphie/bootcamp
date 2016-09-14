f263a94c47bd74c0ebbeebd58d533871e691868f
Alphonsus Ng <ng.alphonsus@gmail.com>
2016-09-13 19:15:10 -0700

    test

--- l23-24-ex5.py
+++ l23-24-ex5.py
@@ -17,6 +17,10 @@ xa_low = np.loadtxt('data/xa_low_food.csv', comments="#")
 x_high, y_high = bc.ecdf(xa_high)
 x_low, y_low = bc.ecdf(xa_low)
 
+x = np.linspace(1600, 2500, 400)
+cdf_high = ss.norm.cdf(x, loc=np.mean(xa_high), scale=np.std(xa_high))
+cdf_low = ss.norm.cdf(x, loc=np.mean(xa_low), scale=np.std(xa_low))
+
 plt.close()
 plt.plot(x_high, y_high, marker='.', linestyle='none',
          markersize=20, alpha=0.5)
@@ -27,11 +31,6 @@ plt.legend(('high food', 'low food'), loc='lower right')
 plt.xlabel('Cross-sectional area (um)')
 plt.ylabel('eCDF')
 
-
-
-x = np.linspace(1600, 2500, 400)
-cdf_high = ss.norm.cdf(x, loc=np.mean(xa_high), scale=np.std(xa_high))
-cdf_low = ss.norm.cdf(x, loc=np.mean(xa_low), scale=np.std(xa_low))
 
 plt.plot(x, cdf_high, color='gray', marker='.', linestyle='none')
 plt.plot(x,cdf_low, color='gray', marker='.', linestyle='none')
